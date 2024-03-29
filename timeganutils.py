4ad2d0056bb21433a4ced82b2c2399c7ca4729a6
harim <hrjeong04@kaist.ac.kr>
2024-03-29 05:08:30 +0000

    create dataloader
    dataloader.py
    	implement Stock, Sine, SyntheticTS datasets and DataloaderHandler
    	please, check the dataloader_tuto.ipynb file

--- timeganutils.py
+++ timeganutils.py
@@ -3,58 +3,6 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-# taken from the original paper https://github.com/jsyoon0823/TimeGAN/blob/master/utils.py
-def sine_data_generation (no, seq_len, dim):
-  """Sine data generation.
-  
-  Args:
-    - no: the number of samples
-    - seq_len: sequence length of the time-series
-    - dim: feature dimensions
-    
-  Returns:
-    - data: generated data
-  """  
-  # Initialize the output
-  data = list()
-
-  # Generate sine data
-  for i in range(no):      
-    # Initialize each time-series
-    temp = list()
-    # For each feature
-    for k in range(dim):
-      # Randomly drawn frequency and phase
-      freq = np.random.uniform(0, 0.1)            
-      phase = np.random.uniform(0, 0.1)
-          
-      # Generate sine signal based on the drawn frequency and phase
-      temp_data = [np.sin(freq * j + phase) for j in range(seq_len)] 
-      temp.append(temp_data)
-        
-    # Align row/column
-    temp = np.transpose(np.asarray(temp))        
-    # Normalize to [0,1]
-    temp = (temp + 1)*0.5
-    # Stack the generated data
-    data.append(temp)
-                
-  return data
-
-# Data normalization
-def MinMaxScaler(data):
-  """Min Max normalizer.
-  
-  Args:
-    - data: original data
-  
-  Returns:
-    - norm_data: normalized data
-  """
-  numerator = data - np.min(data, 0)
-  denominator = np.max(data, 0) - np.min(data, 0)
-  norm_data = numerator / (denominator + 1e-7)
-  return norm_data
 
 def random_generator (batch_size, z_dim, T_mb, max_seq_len):
   """Random vector generation. (Generates a latent vector that is used in the Generator.)
