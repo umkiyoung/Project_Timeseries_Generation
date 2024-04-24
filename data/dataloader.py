f1eb6b31af274f5b4a8ab9015e3ae0e3d2076204
harim <hrjeong04@kaist.ac.kr>
2024-04-24 13:21:44 +0000

    developing

--- data/dataloader.py
+++ data/dataloader.py
@@ -4,19 +4,15 @@ from utils.utils import instantiate_from_config
 def dataloader_info(config):
     batch_size = config['dataloader']['batch_size']
     jud = config['dataloader']['shuffle']
-    num_workers = config["dataloader"]["num_workers"]
     dataset = instantiate_from_config(config['dataloader']['train_dataset'])
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=batch_size,
                                              shuffle=jud,
-                                             num_workers=num_workers,
                                              pin_memory=True,
                                              sampler=None,
                                              drop_last=jud)
-    dl_info = {
-    'dataloader': dataloader,
-    'dataset': dataset
-    }
+    dl_info = {'dataloader': dataloader,
+               'dataset': dataset}
 
     return dl_info
 
